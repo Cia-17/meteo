7f6888c771c40bfe7cbaca1c1b820bf7c984a037
Cia-17 <kyeibaffourcia@gmail.com>
2025-06-25 18:41:59 -0700

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 function refreshWeather(response){
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
-    temperatureElement.innerHTML = Math.round(temperature)
+    let descriptionElement = document.querySelector("#description")
     let cityElement = document.querySelector("#weather-app-city")
-    cityElement.innerHTML = response.data.city;
+    let humidityElement = document.querySelector("#humidity")
+    let windSpeedElement = document.querySelector("#wind-speed")
+    let timeElement = document.querySelector("#time")
+    let date = new Date(response.data.time * 1000);
 
+
+    timeElement.innerHTML = formatDate(date)
+    windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    temperatureElement.innerHTML = Math.round(temperature)
+    cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+
+}
+
+function formatDate(date){
+    let minutes = date.getMinutes()
+    let hours = date.getHours()
+
+    let days = [
+        "Sunday", 
+        "Monday", 
+        "Tuesday", 
+        "Wednesday",
+        "Thursday", 
+        "Friday", 
+        "Saturday"]
+
+    let day = days[date.getDay()]
+    if (minutes<10){
+        minutes = `0${minutes}`
+    }
+
+    return `${day} ${hours}:${minutes}`
+
+
+
+    
 }
 
 
@@ -26,8 +62,6 @@ function handleSearchSubmit(event){
      searchCity(searchInput.value)
      
 }
-
-
 
 
 let searchFormElement = document.querySelector("#search-form");
