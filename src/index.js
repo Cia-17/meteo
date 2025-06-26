fc53806f1dda29341a86a95803bc35fab9480e8a
Cia-17 <kyeibaffourcia@gmail.com>
2025-06-25 19:41:02 -0700

    Made changes to temperature

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ function refreshWeather(response){
     let windSpeedElement = document.querySelector("#wind-speed")
     let timeElement = document.querySelector("#time")
     let date = new Date(response.data.time * 1000);
-
+    let iconElement = document.querySelector("#icon")
 
     timeElement.innerHTML = formatDate(date)
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"  />`
     windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     temperatureElement.innerHTML = Math.round(temperature)
